fdaabcd15467ebef509ad66c7c8da297e025bcc2
Jan Werner <jan.werner@axxessio.com>
2023-12-05 18:41:30 +0100

    C#

---
+++ dotnet/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using NATS.Client.Core;
+using NATS.Client.JetStream;
+using NATS.Client.ObjectStore;
+
+
+
+Console.WriteLine("Hello nats-perf");
+
+const string obs_bucket = "dotnet_bucket";
+
+await using var nats = new NatsConnection();
+var js = new NatsJSContext(nats);
+
+var obj = new NatsObjContext(js);
+var store = await obj.CreateObjectStore(obs_bucket);
+
+while (true)
+{
+    await store.PutAsync(
+        "/Users/jan.werner/Downloads/wochendaemmerung.opus",
+        File.OpenRead("/Users/jan.werner/Downloads/wochendaemmerung.opus")
+    );
+}
+
+
+
